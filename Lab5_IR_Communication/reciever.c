e3ed380b9e7deb8b5873cdddd90eca76480330a3
Yoshiesaur <zarek.moreno01@utrgv.edu>
2025-04-28 09:12:55 -0500

    Finished receiver and sender code

--- Lab5_IR_Communication/reciever.c
+++ Lab5_IR_Communication/reciever.c
@@ -1,22 +1,103 @@
+// File: rx_ir.c
+// Target: PIC16F676 @ 4 MHz internal oscillator
+// Infrared 4-bit receiver (no PWM)
+
 #include <xc.h>
 #include <stdint.h>
 
-// oscillator frequency for __delay_us/__delay_ms
 #define _XTAL_FREQ 4000000UL
 
 // CONFIGURATION BITS
-#pragma config FOSC   = INTRCIO   // Internal OSC, RA4/OSC2 & RA5/OSC1 as I/O
-#pragma config WDTE   = OFF       // Watchdog Timer disabled
-#pragma config PWRTE  = OFF       // Power-up Timer disabled
-#pragma config MCLRE  = OFF       // RA3/MCLR pin enabled as digital input
-#pragma config BOREN  = OFF       // Brown-out Reset disabled
-#pragma config LVP    = OFF       // Low-Voltage Programming disabled
-#pragma config CPD    = OFF       // Data EEPROM Code Protection disabled
-#pragma config CP     = OFF       // Flash Program Memory Code Protection disabled
+#pragma config FOSC   = INTRCIO
+#pragma config WDTE   = OFF
+#pragma config PWRTE  = OFF
+#pragma config MCLRE  = OFF
+#pragma config BOREN  = OFF
+#pragma config LVP    = OFF
+#pragma config CPD    = OFF
+#pragma config CP     = OFF
 
-//Function Protypes
+// I/O definitions
+#define IR_RX_PORT  PORTAbits.RA2
+#define IR_RX_TRIS  TRISAbits.TRISA2
+#define RX_LED_LAT  LATC
+#define RX_LED_TRIS TRISC
+#define RX_LED_MASK 0x0F    // RC0..RC3 outputs
 
+// Timing & thresholds (µs)
+#define START_PULSE_US   1000
+#define START_MIN        800    // acceptable start width range
+#define START_MAX       1200
+#define BIT_THRESHOLD    375    // mid-point between 250 & 500
+#define BIT_GAP_US       500
 
-int main(void){
+// prototypes
+void init_ports(void);
+void init_timers(void);
+uint8_t receive_frame(void);
 
+void main(void) {
+    uint8_t rx;
+    init_ports();
+    init_timers();
+
+    while (1) {
+        rx = receive_frame();
+        if (rx != 0xFF) {
+            RX_LED_LAT = (RX_LED_LAT & ~RX_LED_MASK) | (rx & RX_LED_MASK);
+        }
+    }
+}
+
+void init_ports(void) {
+    ANSEL = 0x00;
+    CMCON = 0x07;
+    VRCON = 0x00;
+
+    // RA2 as input
+    IR_RX_TRIS = 1;
+    // LEDs RC0..RC3 as outputs
+    RX_LED_TRIS &= ~RX_LED_MASK;
+    RX_LED_LAT  = 0;
+}
+
+void init_timers(void) {
+    // Timer0: Fosc/4 = 1 MHz, prescaler 1:1 ⇒ 1 µs tick
+    OPTION_REG = 0b00000000;  // PSA→TMR0, PS=1:1
+    TMR0 = 0;
+}
+
+uint8_t receive_frame(void) {
+    uint8_t i, bit, data = 0;
+    uint8_t width;
+
+    // 1) Wait for start-pulse rising edge
+    while (!IR_RX_PORT) { }
+    TMR0 = 0;
+    // measure high-time
+    while (IR_RX_PORT) { }
+    width = TMR0;
+    if (width < START_MIN || width > START_MAX) {
+        return 0xFF;    // invalid frame
+    }
+    // gap
+    __delay_us(BIT_GAP_US);
+
+    // 2) Read 4 bits MSB first
+    for (i = 0; i < 4; i++) {
+        // wait rising
+        while (!IR_RX_PORT) { }
+        TMR0 = 0;
+        // measure high-time
+        while (IR_RX_PORT) { }
+        width = TMR0;
+
+        bit = (width > BIT_THRESHOLD) ? 1 : 0;
+        data |= (bit << (3 - i));
+
+        // gap
+        __delay_us(BIT_GAP_US);
+    }
+
+    return data;
 }
--- Lab5_IR_Communication/sender.c
+++ Lab5_IR_Communication/sender.c
@@ -1,7 +1,10 @@
+// File: tx_ir.c
+// Target: PIC16F676 @ 4 MHz internal oscillator
+// Infrared 4-bit transmitter (no PWM)
+
 #include <xc.h>
 #include <stdint.h>
 
-// oscillator frequency for __delay_us/__delay_ms
 #define _XTAL_FREQ 4000000UL
 
 // CONFIGURATION BITS
@@ -14,9 +17,64 @@
 #pragma config CPD    = OFF       // Data EEPROM Code Protection disabled
 #pragma config CP     = OFF       // Flash Program Memory Code Protection disabled
 
-//Function Protypes
+// I/O definitions
+#define TX_PORT     PORTA
+#define TX_TRIS     TRISA
+#define TX_MASK     0x0F        // RA0..RA3 inputs
+#define IR_TX_LAT   LATAbits.LATA5
+#define IR_TX_TRIS  TRISAbits.TRISA5
 
+// Timing (µs)
+#define START_PULSE_US 1000
+#define START_GAP_US    500
+#define BIT0_PULSE_US   250
+#define BIT1_PULSE_US   500
+#define BIT_GAP_US      500
 
-int main(void){
+// prototypes
+void init_ports(void);
+void send_frame(uint8_t data);
 
+void main(void) {
+    init_ports();
+
+    while (1) {
+        uint8_t data = TX_PORT & TX_MASK;
+        send_frame(data);
+        __delay_ms(50);        // small pause between frames
+    }
+}
+
+void init_ports(void) {
+    ANSEL = 0x00;     // all digital
+    CMCON = 0x07;     // comparators off
+    VRCON = 0x00;     // voltage ref off
+
+    // inputs RA0..RA3
+    TX_TRIS |= TX_MASK;
+    // IR drive RA5
+    IR_TX_TRIS = 0;
+    IR_TX_LAT  = 0;
+}
+
+void send_frame(uint8_t data) {
+    uint8_t i;
+
+    // 1) Start pulse
+    IR_TX_LAT = 1;
+    __delay_us(START_PULSE_US);
+    IR_TX_LAT = 0;
+    __delay_us(START_GAP_US);
+
+    // 2) 4 data bits, MSB first
+    for (i = 0; i < 4; i++) {
+        IR_TX_LAT = 1;
+        if (data & (1 << (3 - i))) {
+            __delay_us(BIT1_PULSE_US);
+        } else {
+            __delay_us(BIT0_PULSE_US);
+        }
+        IR_TX_LAT = 0;
+        __delay_us(BIT_GAP_US);
+    }
 }
